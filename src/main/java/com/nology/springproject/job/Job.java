f54d18b64ccadddbb7670141e7fce80b487add5a
Jed Habush <90814324+jedhabush@users.noreply.github.com>
2022-10-31 12:10:08 +1100

    Add files via upload
    Added The basic relationship between temp and job

--- src/main/java/com/nology/springproject/job/Job.java
+++ src/main/java/com/nology/springproject/job/Job.java
@@ -2,10 +2,16 @@ package com.nology.springproject.job;
 
 import java.time.LocalDate;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.nology.springproject.temp.Temp;
 
 @Entity
 
@@ -16,12 +22,28 @@ public class Job {
 	private String name;
 	private LocalDate startDate;
 	private LocalDate endDate;
+	
+	
+	//relationship between temp and job 
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "temp_id")
+	@JsonBackReference
+	private Temp temp;
+	
 	// constructor -> using getters and setters
 	public Job(String name, LocalDate startDate, LocalDate endDate) {
 		this.setName(name);
 		this.setStartDate(startDate);
 		this.setEndDate(endDate);
 	}
+	// temp relationship setters and getters
+	public Temp getTemp() {
+		return temp;
+	}
+	public void setTemp(Temp temp) {
+		this.temp = temp;
+	}
+	
 	// default constructor 
 	public Job() {
 		
--- src/main/java/com/nology/springproject/temp/Temp.java
+++ src/main/java/com/nology/springproject/temp/Temp.java
@@ -1,9 +1,15 @@
 package com.nology.springproject.temp;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.nology.springproject.job.Job;
 
 @Entity
 public class Temp {
@@ -14,6 +20,13 @@ public class Temp {
 private long id;
 private String firstName;
 private String lastName;
+
+
+//relationship between Temp and Job
+
+@OneToMany(mappedBy = "temp" , cascade = CascadeType.ALL)
+private List<Job> jobList;
+
 public Temp(String firstName, String lastName) {
 	
 	this.setFirstName(firstName);
@@ -47,6 +60,15 @@ public String getLastName() {
 
 public void setLastName(String lastName) {
 	this.lastName = lastName;
+}
+
+//relationship methods
+
+public List<Job> getJobArr() {
+	return jobList;
+}
+public void setJobArr(List<Job> jobList) {
+	this.jobList = jobList;
 }
 
 
